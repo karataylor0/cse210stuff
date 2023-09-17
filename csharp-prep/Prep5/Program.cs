54034e934695ca636ee5bd714993e69675ca0b37
Kara Taylor <tay21020@byui.edu>
2023-09-16 21:49:38 -0600

    Name and Square

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.IO.Compression;
+using System.Numerics;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep5 World!");
+        //DisplayWelcome - displays "welcome to the program!"
+        static void DisplayWelcome ()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+        //PromptUserName
+        static string PromptUserName ()
+        {
+            Console.WriteLine("What is your name?");
+            string username = Console.ReadLine();
+            return username;
+        }
+        //PromptUserNumber
+        static int PromptUserNumber ()
+        {
+            Console.WriteLine("What is your favorite number?");
+            int userNumber = int.Parse(Console.ReadLine());
+            return userNumber;
+        }
+        //SquareNumber
+        static int SquareNumber (int number)
+        {
+            int squaredNumber = number * number;
+            return squaredNumber;
+        }
+        //DisplayResult
+        static void DisplayResult(string username, int squared)
+        {
+            Console.WriteLine($"{username}, the square of your favorite number is {squared}.");       
+        }
+
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int originalNumber = PromptUserNumber();
+        int squaredNumber = SquareNumber(originalNumber);
+        DisplayResult(userName, squaredNumber);
     }
 }
