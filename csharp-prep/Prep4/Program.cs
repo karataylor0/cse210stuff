1c16338da587d153c427a5b1e6fd500291f38053
Kara Taylor <tay21020@byui.edu>
2023-09-16 21:28:26 -0600

    List of numbers.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+Console.WriteLine("Enter a list of numbers; type 0 when finished.");
+
+List<int> numbers = new List<int>();
+
+int typedNumber = -1;
+while (typedNumber != 0)
 {
-    static void Main(string[] args)
+    Console.WriteLine("Enter number:");
+    if (typedNumber != 0)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        typedNumber = int.Parse(Console.ReadLine());
+        numbers.Add(typedNumber);
     }
 }
+
+int largestNumber = 0;
+int sum = 0;
+foreach (int number in numbers)
+{
+    sum += number;
+    if (number > largestNumber)
+    {
+        largestNumber = number;
+    }
+}
+Console.WriteLine($"The sum is :{sum}");
+
+int amount = numbers.Count;
+int average = sum / amount;
+Console.WriteLine($"The average is: {average}");
+
+Console.WriteLine($"The largest number is: {largestNumber}");
