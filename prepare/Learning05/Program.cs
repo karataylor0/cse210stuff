265355431c3708895f3df03c80dba84cf5dc32b1
Kara Taylor <tay21020@byui.edu>
2023-10-12 14:59:11 -0600

    shapes

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapeList = new List<Shape>();
+        
+        
+
+        Square square1 = new Square(10, "yellow");
+
+        shapeList.Add(square1);
+        Console.WriteLine(square1.GetColor());
+        Console.WriteLine(square1.GetArea());
+
+        Rectangle rectangle1 = new Rectangle(4, 5, "blue");
+
+        shapeList.Add(rectangle1);
+        Console.WriteLine(rectangle1.GetColor());
+        Console.WriteLine(rectangle1.GetArea());
+
+        Circle circle1 = new Circle(1, "red");
+
+        shapeList.Add(circle1);
+        Console.WriteLine(circle1.GetColor());
+        Console.WriteLine(circle1.GetArea());
+
+
+        foreach (Shape shape in shapeList)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
     }
 }
